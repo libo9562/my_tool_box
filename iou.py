673e6d64c4c3eaae2bd9aff4bf8afcdfa4fe333c
libo9562 <libo9562@gmail.com>
2018-10-11 14:27:23 -0400

    Add files via upload

---
+++ iou.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+import numpy as np
+
+def get_iou_vector(A, B):
+    # Numpy version
+    assert A.shape == B.shape
+    metric = 0.0
+    for t, p in zip(A,B):
+        true = np.sum(t)
+        pred = np.sum(p)
+        
+        # deal with empty mask first
+        if true == 0:
+            metric += (pred == 0)
+            continue
+        
+        # non empty mask case.  Union is never empty 
+        # hence it is safe to divide by its number of pixels
+        intersection = np.sum(t * p)
+        union = true + pred - intersection
+        iou = intersection / union
+        
+        # iou metrric is a stepwise approximation of the real iou over 0.5
+        iou = np.floor(max(0, (iou - 0.45)*20)) / 10
+        metric += iou
+        
+    # teake the average over all images in batch
+    metric /= A.shape[0]
+    return metric
+
+def my_iou_metric(label, pred):
+    return tf.py_func(get_iou_vector, [label, pred>0.5], tf.float64)
+
+def my_iou_metric_2(label, pred):
+    return tf.py_func(get_iou_vector, [label, pred >0], tf.float64)
---
+++ train_helper.py
@@ -0,0 +1,13 @@
+import matplotlib.plot as plt
+
+def plot_history(hist):
+    height = (len(hist.history)+1)//4+1
+    figs, axes = plt.subplots(height, 2, figsize=(15,5*height))
+    keys = list(filter(lambda x: x.startswith('val'),history.history.keys()))
+    for i,key in enumerate(keys):
+        axes[i//2,i%2].plot(hist.epoch, hist.history[key[4:]], label="Train "+key[4:])
+        axes[i//2,i%2].plot(hist.epoch, hist.history[key], label="Validation "+key)
+        axes[i//2,i%2].legend()
+    i += 1
+    axes[i//2,i%2].plot(hist.epoch, hist.history['lr'], label="learning rate")
+    axes[i//2,i%2].legend()
