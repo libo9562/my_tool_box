a779db2d1991b18b7e08f586cccca020cb42f1f5
libo9562 <libo9562@gmail.com>
2018-10-11 14:36:11 -0400

    Update train_helper.py

--- train_helper.py
+++ train_helper.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 def plot_history(hist):
     height = (len(hist.history)+1)//4+1
     figs, axes = plt.subplots(height, 2, figsize=(15,5*height))
-    keys = list(filter(lambda x: x.startswith('val'),history.history.keys()))
+    keys = list(filter(lambda x: x.startswith('val'), hist.history.keys()))
     for i,key in enumerate(keys):
         axes[i//2,i%2].plot(hist.epoch, hist.history[key[4:]], label="Train "+key[4:])
         axes[i//2,i%2].plot(hist.epoch, hist.history[key], label="Validation "+key)
